1907753e015b209e362d06ebff86c6e07b1b235a
harshad1234u <harshadambi20@gmail.com>
2026-06-28 22:40:43 +0530

    Fix Grad-CAM eager execution to support nested Sequential layers

--- model/gradcam.py
+++ model/gradcam.py
@@ -7,53 +7,53 @@ def make_gradcam_heatmap(img_array, model, last_conv_layer_name=None, pred_index
     """
     Generates a Grad-CAM heatmap for the given image array and model.
     """
-    # Find the global average pooling layer dynamically
-    gap_layer = None
-    for layer in model.layers:
-        if "global_average_pooling2d" in layer.name.lower():
-            gap_layer = layer
-            break
-            
-    if gap_layer is None:
-        raise ValueError("Could not find global_average_pooling2d layer in the model architecture.")
-        
-    grad_model = tf.keras.models.Model(
-        [model.inputs], 
-        [gap_layer.input, model.output]
-    )
-
-    # 2. Compute the gradient of the top predicted class for our input image
-    # with respect to the activations of the last conv layer
+    x = tf.convert_to_tensor(img_array, dtype=tf.float32)
+    last_conv_layer_output = None
+    
     with tf.GradientTape() as tape:
-        last_conv_layer_output, preds = grad_model(img_array)
-        if pred_index is None:
-            # We are doing binary classification, so preds is just a single probability
-            # The "score" we want to maximize is the probability of the predicted class.
-            # If preds > 0.5, we want to explain why it's REAL (class 1).
-            # If preds <= 0.5, we want to explain why it's FAKE (class 0).
-            # For class 1, maximize preds. For class 0, maximize (1 - preds).
-            pred_value = preds[0][0]
-            if pred_value > 0.5:
-                class_channel = preds
-            else:
-                class_channel = 1.0 - preds
-        else:
+        for layer in model.layers:
+            if isinstance(layer, tf.keras.layers.InputLayer):
+                continue
+            # Skip nested Sequential models like DataAugmentation which are not needed for inference
+            if "augmentation" in layer.name.lower() or isinstance(layer, tf.keras.Sequential):
+                continue
+                
+            if "global_average_pooling" in layer.name.lower():
+                last_conv_layer_output = x
+                tape.watch(last_conv_layer_output)
+                
+            x = layer(x, training=False)
+            
+        preds = x
+        
+        if last_conv_layer_output is None:
+            raise ValueError("Could not find global_average_pooling2d layer in the model architecture.")
+            
+        # We are doing binary classification, so preds is just a single probability
+        pred_value = preds[0][0]
+        if pred_value > 0.5:
             class_channel = preds
+        else:
+            class_channel = 1.0 - preds
 
-    # 3. This is the gradient of the output neuron with respect to the output feature map
+    # Compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
     grads = tape.gradient(class_channel, last_conv_layer_output)
 
-    # 4. Global average pooling of gradients
+    if grads is None:
+        raise ValueError("Gradient calculation failed. Ensure the model structure connects to the output.")
+
+    # Global average pooling of gradients
     pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
 
-    # 5. Multiply each channel in the feature map array by "how important this channel is"
+    # Multiply each channel in the feature map array by "how important this channel is"
     last_conv_layer_output = last_conv_layer_output[0]
     # We add an axis to pooled_grads to broadcast
     pooled_grads = pooled_grads[..., tf.newaxis]
     heatmap = last_conv_layer_output @ pooled_grads
     heatmap = tf.squeeze(heatmap)
 
-    # 6. Normalize the heatmap between 0 and 1
+    # Normalize the heatmap between 0 and 1
     heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
     return heatmap.numpy()
 
